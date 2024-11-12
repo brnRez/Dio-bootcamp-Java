a6a97060d4037f155f716ca6f4fd40fba797299c
Bruno Maciel <maciel2bruno@gmail.com>
2024-11-11 21:31:54 -0300

    Criada para explorar os tipos de Variáveis, referente a aula de Sintaxe Básica.

---
+++ Java-Basico/src/edu/brunao/tiposVariaveis/TiposVariaveis.java
@@ -0,0 +1,19 @@
+package edu.brunao.tiposVariaveis;
+
+public class TiposVariaveis {
+
+    String primeiroNome = "Brunao";
+
+    double salarioMinimo = 2500;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+    int numero = 5;
+
+    final double VALOR_DE_PI = 3.14;
+
+
+
+}
