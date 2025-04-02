f42935e3098d0c4dadd84a9ffe8e96d06429f42c
Bruno Maciel <maciel2bruno@gmail.com>
2025-04-02 13:09:24 -0300

    Aula List - Operações básicas

---
+++ JavaCollections/src/edu/brunao/List/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,21 @@
+package edu.brunao.List.OperacoesBasicas;
+
+public class Tarefa {
+    //atributo
+
+    private String descricao;
+
+    public Tarefa(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString(){
+        return descricao;
+    }
+
+}
