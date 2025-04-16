2165ea0eb513d93aa1f7dca81374fc967c8772cf
Bruno Maciel <maciel2bruno@gmail.com>
2025-04-16 14:32:50 -0300

    Feat: Entidade Transacao.

--- Desafios/BancoDigitalOO/Transacao.java
+++ Desafios/BancoDigitalOO/Transacao.java
@@ -1,5 +1,22 @@
 package Desafios.BancoDigitalOO;
 
+import java.util.Date;
+
 public class Transacao {
+    //atributos
+    private Date data;
+    private String tipo;
+    private double valor;
+
+    public Transacao(String tipo, double valor){
+        this.data = new Date();
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public String getResumo(){
+        return data + " - " + tipo + ": R$ " + valor;
+    }
+    
     
 }
