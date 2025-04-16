80aa3b9bcf799bcb44c6ee0f81f96fc5989f3840
Bruno Maciel <maciel2bruno@gmail.com>
2025-04-16 14:01:15 -0300

    refactor: removed Banco.java and added Main.java

--- Desafios/BancoDigitalOO/Banco.java
+++
@@ -1,6 +0,0 @@
-package Desafios.BancoDigitalOO;
-
-public class Banco {
-    
-
-}
---
+++ Desafios/BancoDigitalOO/Main.java
@@ -0,0 +1,7 @@
+package Desafios.BancoDigitalOO;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
