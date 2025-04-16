3fff30827a965d03f50d2f18024a6e32a996f58c
Bruno Maciel <maciel2bruno@gmail.com>
2025-04-16 14:31:38 -0300

    Feat: Entidade cliente e Classe Abstrata Conta construídas.

--- Desafios/BancoDigitalOO/Cliente.java
+++ Desafios/BancoDigitalOO/Cliente.java
@@ -1,5 +1,37 @@
 package Desafios.BancoDigitalOO;
 
 public class Cliente {
+    //atributos
+    private String nome;
+    private String cpf;
+    private String email;
+
+
+    public Cliente(String nome, String cpf, String email){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    
+    public String getCpf(){
+        return cpf;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Cliente [nome=" + nome + ", cpf=" + cpf + ", email=" + email + "]";
+    }
+
+    
+
 
 }
--- Desafios/BancoDigitalOO/Conta.java
+++ Desafios/BancoDigitalOO/Conta.java
@@ -1,5 +1,51 @@
 package Desafios.BancoDigitalOO;
 
-public class Conta {
+import java.util.ArrayList;
+import java.util.List;
 
+public abstract class Conta {
+
+    //atributos
+    protected String numero;
+    protected double saldo;
+    protected Cliente cliente;
+    protected List<Transacao> historico = new ArrayList<>();
+
+    public Conta (String numero, Cliente cliente){
+        this.numero = numero;
+        this.cliente = cliente;
+        this.saldo = 0;
+    }
+
+    public void depositar (double valor) {
+        saldo += valor;
+        historico.add(new Transacao("Deposito", valor));
+    }
+    
+    public void sacar(double valor){
+        if (valor <= saldo){
+            saldo -= valor;
+            historico.add(new Transacao("Saque", valor));
+        }
+    }
+
+
+    public void transferir (Conta destino, double valor){
+        if (valor <= saldo){
+            this.saldo -= valor;
+            destino.depositar(valor);
+            historico.add(new Transacao("Transferência", valor));
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    
+    public abstract void imprimirExtrato();
 }
