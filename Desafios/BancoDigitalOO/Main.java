158afebd58fceefde521ebbc0cd464d9d49fa213
Bruno Maciel <maciel2bruno@gmail.com>
2025-04-16 14:33:43 -0300

    Feat: Construção da Classe Main para teste das funções.

--- Desafios/BancoDigitalOO/Main.java
+++ Desafios/BancoDigitalOO/Main.java
@@ -2,6 +2,18 @@ package Desafios.BancoDigitalOO;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Cliente cliente1 = new Cliente("Brunao", "123.456.789-00", "brunao@email.com");
+        ContaCorrente cc = new ContaCorrente("0001", cliente1, 500);
+        ContaPoupanca cp = new ContaPoupanca("0002", cliente1, 0.01);
+
+        cc.depositar(1000);
+        cc.sacar(200);
+        cc.transferir(cp, 300);
+
+        cp.aplicarRendimento();
+
+        cc.imprimirExtrato();
+        System.out.println();
+        cp.imprimirExtrato();
     }
 }
